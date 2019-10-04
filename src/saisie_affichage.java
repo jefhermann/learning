dd02872f2ff6f0b78feed5aab2ec9e9e0f4113ab
Jef <hermannjef@gmail.com>
2019-10-04 11:09:53 +0000

    saisie et affichage
    -le cas d'une chaine de caractere
    
    -le cas d'un entier

---
+++ src/saisie_affichage.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class saisie_affichage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		
+		//la saisie et affichage pour une chaine de caractaire
+		System.out.print("Entrez votre nom :");
+		String i = sc.nextLine();
+		System.out.println("Vous ave saisi:"+i);
+		
+		//la saisie et affichage un entier
+		System.out.print("\n \n Saisisser un entier :");
+		
+
+	}
+
+}
