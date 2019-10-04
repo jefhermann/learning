0cbcd0c4eedcf0124add2b893ba442543060d2f9
Jef <hermannjef@gmail.com>
2019-10-04 09:52:34 +0000

    first commit
    initialisation du projet

---
+++ src/conditions.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class conditions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		System.out.print("Entrez votre nom :");
+		String i = sc.nextLine();
+		System.out.println("Vous ave saisi:"+i /n/n);
+		
+		
+	}
+
+}
